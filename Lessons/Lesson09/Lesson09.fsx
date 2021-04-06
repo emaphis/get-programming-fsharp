644a2535a639a81612652b40084850087db156b1
emaphis <emaphis85@gmail.com>
2021-04-05 21:06:03 -0400

    Lesson 9 - tuples.

--- Lessons/Lesson09/Lesson09.fsx
+++ Lessons/Lesson09/Lesson09.fsx
@@ -38,9 +38,63 @@ let parse (person: string) =
     name, game, score
 
 let player, game, score = parse "Mary Asteroids 2300"
-
+;;;
 
 /// 9.2.1  When should I use tuples?  - pg 105
 
+/// 9.3 More complex tuples  - pg 106
+
+/// 9.3.1  Tupe type signatures
+ 
+ let nameAndAge0 = ("Joe", "Blogs"), 28
+// val nameAndAge : (string * string) * int
 
 
+/// 9.3.2  Nested tuples
+
+/// Listing 9.4 Returning more-complex arbitrary data pairs in F#
+
+let nameAndAge = ("Joe", "Blogs"), 28
+let name1, age1 = nameAndAge
+let (forename1, surname1), theAge = nameAndAge
+
+
+/// 9.3.3 Wildcards  - pg 107
+/// Listing 9.5  Using wildcars with tuples 
+
+let nameAndAge1 = "Jane", "Smith", 25
+let forename2, surname2, _ = nameAndAge1
+
+
+/// 9.3.4  Type inference with tuples
+/// Listing 9.6 
+let explicit : int * int = 10, 5
+let implicit = 10, 5  // type infered
+;;
+let addNumbers0 arguments =
+    let a, b = arguments  // destructuring
+    a + b
+
+7 = addNumbers0(3, 4)
+
+
+/// Listing 9.7  Generalized functions with tuples
+let addNumbers2 arguments =
+    let a, b, c, _ = arguments
+    a + b
+
+
+/// 9.4  Tuples best practices  - pg 108
+
+/// 9.4.1  Tuples and the BCL
+
+/// Listing 9.8 Implicit mapping of out parameters to tuples  - pg 109
+// var number = "123";
+// var result = 0;
+// var parsed = Int32.TryParse(number, out, result)
+
+let number = "456"
+let result, parsed = System.Int32.TryParse(number)
+
+
+/// 9.4.2  When not to use tuples
--- WinForms/readme.txt
+++ WinForms/readme.txt
@@ -3,3 +3,10 @@ Example of as F# WinForm project for Dotnet 5.0
 I spent all afternoon wrestling with Lesson 4 trying to get a F# Winform project to work.
 I found: https://fsharpmonologue.blogspot.com/2019/12/advent-2019-f-advent-2019-revisiting.html
 With examples at:  https://github.com/eriawan/netcore-fsharp-sample
+
+Also see: 
+https://github.com/mouadcherkaoui/FSharpWPF
+https://github.com/bent-rasmussen/FsharpWpf
+
+winforms
+https://github.com/Krzysztof-Cieslak/WinForms-FSharp-Sample
