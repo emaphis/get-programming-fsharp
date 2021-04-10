0e858c3677fd7b9a1d9ac83b7008f9d4479502ec
emaphis <emaphis85@gmail.com>
2021-04-09 21:25:19 -0400

    Complete Lesson 14, Capstone 2 Implemented main function

--- Lessons/Lesson14-Capstone2/Capstone2/Program.fs
+++ Lessons/Lesson14-Capstone2/Capstone2/Program.fs
@@ -1,13 +1,45 @@
-// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
+// Main banking app
 
 open System
+open Capstone2.Domain
+open Capstone2.Operations
+open Capstone2.Auditing
 
-// Define a function to construct a message to print
-let from whom =
-    sprintf "from %s" whom
+// User interface functions
+let getAccountInfo(argv: string[]) =
+    let customer = { Name = argv.[0] }
+    let balance = decimal(argv.[1])
+    { AccountId = Guid.NewGuid(); Owner = customer; Balance = balance;  }
+
+let getAction(account) =
+    printfn "\nCurrent balance is $%M" account.Balance
+    printfn "(d)eposit, (w)ithdraw or e(x)it"
+    Console.ReadLine()
+
+let getAmount() =
+    printf "\nAmount: "
+    decimal(Console.ReadLine())
+
+
+/// Custom decorated auditing functions
+let withdrawC  = withdraw |> auditAs "withdraw" consoleAudit
+let depositC  = deposit |> auditAs "deposit" consoleAudit
+
 
 [<EntryPoint>]
 let main argv =
-    let message = from "F#" // Call the function
-    printfn "Hello world %s" message
+    let mutable account = getAccountInfo(argv)
+
+    while true do
+        let action = getAction(account)
+        if action = "x" then Environment.Exit 0
+        let amount = getAmount()
+        
+        account <-
+            if action = "d" then account |> depositC amount
+            elif action = "w" then account |> withdrawC amount
+            else account
+
+    printfn "Final amount: $%O" account.Balance
+
     0 // return an integer exit code
