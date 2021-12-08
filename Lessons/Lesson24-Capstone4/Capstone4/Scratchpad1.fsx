c8870b22bb83e564bbe71a345f342a674574644a
emaphis <emaphis85@gmail.com>
2021-12-08 16:39:53 -0500

    Updating the apps Command model

--- Lessons/Lesson24-Capstone4/Capstone4/Scratchpad1.fsx
+++ Lessons/Lesson24-Capstone4/Capstone4/Scratchpad1.fsx
@@ -11,14 +11,27 @@ open System
 open System.IO
 
 
+// 24.2.2 Adding a command handler with discriminated unions - pg. 286
 
-/// Checks whether the command is one of (d)eposit, (w)ithdraw, or e(x)it
-let isValidCommand command =
-    command = 'd' || command = 'w' || command = 'x'
+type BankOperation = Deposit | Withdraw
+type Command = AccountCommand of BankOperation | Exit
 
+let tryGetBankOperation command =
+    match command with
+    | AccountCommand op -> Some op
+    | Exit -> None
+
+
+/// a simple function that can convert a char into a Command
+let tryParseCommand command :Command option =
+    match command with
+    | 'd'  -> Some (AccountCommand Deposit)
+    | 'w'  -> Some (AccountCommand Withdraw)
+    | 'x'  -> Some Exit
+    | _    -> None
 
 /// Checks whether the command is the exit command.
-let isStopCommand commamd = commamd = 'x'
+let isStopCommand command = (command = Exit)
 
 
 /// Takes in a command and converts it to a tuple of the command and also an amount
@@ -27,13 +40,19 @@ let getAmount command =
     Console.Write "Enter Amount: "
     command, Console.ReadLine() |> Decimal.Parse
 
-;;
-/// Takes in an account and a (command, amount) tuple. It should then apply
-/// the appropriate action on the account and return the new account back out again
+
+// Listing 24.2 Working with unbounded values  -pg. 286
+(*
 let processCommand account (command, amount) =
     if   command = 'd' then deposit amount account
     elif command = 'w' then withdraw amount account
     else account
+*)
+
+let processCommand account (command, amount) =
+    match command with
+    | Deposit -> deposit amount account
+    | Withdraw -> withdraw amount account
 
 
 // Listing 19.1 Creating a functional pipeline for commands  - pg. 221
@@ -49,13 +68,21 @@ let consoleCommands = seq {
          yield Console.ReadKey().KeyChar
          Console.WriteLine() }
 
+
+// Listing 24.1 The existing command execution pipeline
+(*
 let account =
-    //let commands = [ 'd'; 'w'; 'z'; 'f'; 'd'; 'x'; 'w' ]
-    //commands
     consoleCommands
     |> Seq.filter isValidCommand
     |> Seq.takeWhile (not << isStopCommand)
     |> Seq.map getAmount
     |> Seq.fold processCommand openingAccount
+*)
 
-
+let account =
+    consoleCommands
+    |> Seq.choose tryParseCommand
+    |> Seq.takeWhile (not << isStopCommand)
+    |> Seq.choose tryGetBankOperation
+    |> Seq.map getAmount
+    |> Seq.fold processCommand openingAccount
